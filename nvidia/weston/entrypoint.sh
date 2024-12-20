16075e2df4301aa7a46599a69bc960b5dafea154
shucheng <shucheng@bd-apaas.com>
2024-12-20 17:50:26 +0800

    update

--- nvidia/weston/entrypoint.sh
+++ nvidia/weston/entrypoint.sh
@@ -3,12 +3,13 @@
 # Set the locale
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/tmp}"
 
 # Generate self-signed certificate
 winpr-makecert -r -pe -n "CN=localhost" -b 01/01/2000 -e 01/01/2036 -sv /opt/privatekey.pem -ic /opt/cacert.pem /opt/certificate.pem
 
 # Start dbus
-dbus-run-session --exit-with-session xrdp-sesman &
+dbus-run-session xrdp-sesman &
 
 # Start Weston
 weston &
